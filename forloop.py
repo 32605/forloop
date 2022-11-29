59b226c8bcefd1cd572091ff86ad6fb78f72a12f
32605 <116140798+32605@users.noreply.github.com>
2022-11-29 12:09:42 +0700

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,57 @@
+colourlist = []
+for i in range(5):
+    colour = input("Enter a colour: ")
+    colourlist.append(colour)
+print(colourlist)  -+++
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+3
